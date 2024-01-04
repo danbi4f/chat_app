5504a11c64184653ff86d4da5d8d297ba40cbd73
DanBi <danbi4f@gmail.com>
2024-01-04 01:35:08 +0100

    first commit

---
+++ lib/pages/auth_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Container()
+          ],
+        ),
+      ),
+    );
+  }
+}
