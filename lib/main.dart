bc1d13bbf0e0a368bc51bc0ab12d2fbd2f1ee291
DanBi <danbi4f@gmail.com>
2024-01-07 01:17:15 +0100

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'package:chat_app/firebase_options.dart';
+import 'package:chat_app/pages/chat_screen.dart';
+import 'package:chat_app/pages/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:chat_app/pages/auth_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+var kColorSchame = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 198, 233, 245),
+);
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
@@ -10,10 +24,21 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: AuthScreen(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(colorScheme: kColorSchame),
+      home: Center(
+        child: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return const SplashScreen();
+            }
+            if (snapshot.hasData) {
+              return const ChatScreen();
+            }
+            return const AuthScreen();
+          },
         ),
       ),
     );
--- lib/pages/auth_screen.dart
+++ lib/pages/auth_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -8,15 +11,132 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  var isLogin = true;
+  final formKey = GlobalKey<FormState>();
+  var enteredEmail = '';
+  var enteredPassword = '';
+
+  submit() {
+    final isValid = formKey.currentState!.validate();
+
+    if (isValid) {
+      return;
+    }
+
+    formKey.currentState!.save();
+
+    if (isLogin) {
+      final userData = firebase.signInWithEmailAndPassword(
+          email: enteredEmail, password: enteredPassword);
+    } else {
+      try {
+        final userData = firebase.createUserWithEmailAndPassword(
+            email: enteredEmail, password: enteredPassword);
+      } on FirebaseAuthException catch (error) {
+        if (error.code == 'email-already-in-use') {
+          //.. (obsługa błędu)
+        }
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(error.message ?? 'Authentication failed.'),
+          ),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
       body: Center(
-        child: Column(
-          children: [
-            Container()
-          ],
-        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 100, bottom: 30),
+                width: 200,
+                child: Image.asset('assets/chat.png'),
+              ),
+              Card(
+                margin: const EdgeInsets.all(20),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        //
+                        TextFormField(
+                          validator: (value) {
+                            if (value == null ||
+                                value.trim().isEmpty ||
+                                !value.contains('@')) {
+                              return 'please enter a valid email address.';
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                            label: Text('Email address'),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(20),
+                              ),
+                            ),
+                          ),
+                          onSaved: (newValue) => enteredEmail = newValue!,
+                        ),
+                        //
+                        const SizedBox(height: 20),
+                        //
+                        TextFormField(
+                          validator: (value) {
+                            if (value == null || value.trim().length < 6) {
+                              return 'Password must be at least 6 characters long.';
+                            }
+                            return null;
+                          },
+                          decoration: const InputDecoration(
+                            label: Text('Password'),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(20),
+                              ),
+                            ),
+                          ),
+                          obscureText: true,
+                          onSaved: (newValue) => enteredPassword = newValue!,
+                        ),
+                        //
+                        const SizedBox(height: 20),
+                        //
+                        ElevatedButton(
+                          onPressed: submit,
+                          child: Text(isLogin ? 'Login' : 'Signup'),
+                        ),
+                        //
+                        const SizedBox(height: 20),
+                        //
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              isLogin = !isLogin;
+                            });
+                          },
+                          child: Text(isLogin
+                              ? 'Create an account'
+                              : 'I have an acount'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ], //
+          ), //
+        ), //
       ),
     );
   }
